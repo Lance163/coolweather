9d2220e48e25973691730407203dde446e630d7c
Lance <240457791LLS@gmeail.com>
2017-05-18 22:40:37 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/coolweather/android/db/Province.java
@@ -0,0 +1,29 @@
+package com.example.coolweather.android.db;
+
+/**
+ * Created by Lance on 2017/5/18.
+ */
+
+public class Province {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public String getProvinceName(){
+        return provinceName;
+    }
+    public void setProvinceName(String provinceName){
+        this.provinceName=provinceName;
+    }
+    public int getProvinceCode(){
+        return provinceCode;
+    }
+    public void setProvinceCode(int provinceCode){
+        this.provinceCode=provinceCode;
+    }
+}
